ea20b3cca473f8f48374784b5b612ac77a4c2e74
Jan Weiß <jan@geheimwerk.de>
2017-04-07 23:48:34 +0200

    Adding ObjC2Plist.

---
+++ ObjC2Plist.m
@@ -0,0 +1,20 @@
+/*
+ * ObjC2Plist.m
+ * ObjC2Plist
+ *
+ * Created by Jan Weiß on 2017-04-07
+ * Licensed under the 3-clause BSD License
+ */
+
+#import <stdio.h>
+#import <Foundation/Foundation.h>
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+		// You can test your generated ObjC here.
+		// Try pasting it in, build and check in the debugger or by writing a few lines.
+		
+		return EXIT_SUCCESS;
+	}
+}
